e02a49d6dcadfa51c550f0ec82f5e84552dbd300
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2024-08-09 18:33:26 +0200

    add trails * ggut55 * kat100

--- trails.js
+++ trails.js
@@ -25,5 +25,23 @@ const trails = {
         raceUrl: "https://events.mountainman.de/grossarltal/xl-trail/",
         image: "https://static.funnelcockpit.com/upload/ZLRJ6jjupCGYZciub/b96f4b898148753c4cdd621f9596d7c0.webp",
         track: "https://crunka3.github.io/NextTrailRunning/assets/tracks/t804191689_mountainman%20grossarltal.gpx"
+    },
+    ggt55: {
+        title: "GROSSGLOCKNER TRAIL (GGT 55)",
+        distance: 57,
+        height: 3500,
+        date: "Juli",
+        raceUrl: "https://www.ultratrail.at/deutsch/rennen/grossglockner-trail/",
+        image: "https://image.jimcdn.com/app/cms/image/transf/dimension=640x10000:format=jpg/path/s7418df6cf20055a6/image/i237fbf358d81d9ae/version/1665420925/image.jpg",
+        track: "https://crunka3.github.io/NextTrailRunning/assets/tracks/ggut-grossglockner-ultra-trail-2024-ggt-55-2024.gpx"
+    },
+    katMarathon: {
+        title: "KAT100 Marathon Trail",
+        distance: 51,
+        height: 3250,
+        date: "Samstag 9. August 2025",
+        raceUrl: "https://kat.utmb.world/de/races/marathon-trail",
+        image: "https://res.cloudinary.com/utmb-world/image/upload/q_auto/f_auto/c_fill,g_auto/if_w_gt_1920/c_scale,w_1920/if_end/v1/kat/Race%20Headers/Header_Image_KAT_100_Marathon_Trail_6f3cb4f4bc?_a=ATADJAA0",
+        track: "https://crunka3.github.io/NextTrailRunning/assets/tracks/KAT_100_Marathon_Trail_24_0a3819fac3.gpx"
     }
 }
