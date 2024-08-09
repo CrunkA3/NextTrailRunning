6ea35c9971c3e69ddf14b0fa9c24710d21a8ada6
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2024-08-09 18:03:17 +0200

    add MOUNTAINMAN Großarltal XL

--- trails.js
+++ trails.js
@@ -16,5 +16,14 @@ const trails = {
         raceUrl: "https://www.zugspitz-ultratrail.com/die-rennen/leutasch-trail/",
         image: "https://www.zugspitz-ultratrail.com/wp-content/uploads/2022/11/ZUT_Landscape.jpg",
         track: "https://crunka3.github.io/NextTrailRunning/assets/tracks/ZUT2024_LeutaschTrail.gpx"
+    },
+    grossarltalXl: {
+        title: "MOUNTAINMAN Grossarltal XL",
+        distance: 50,
+        height: 2703,
+        date: "Juni",
+        raceUrl: "https://events.mountainman.de/grossarltal/xl-trail/",
+        image: "https://static.funnelcockpit.com/upload/ZLRJ6jjupCGYZciub/b96f4b898148753c4cdd621f9596d7c0.webp",
+        track: "https://crunka3.github.io/NextTrailRunning/assets/tracks/t804191689_mountainman%20grossarltal.gpx"
     }
 }
